b96578389d30ac1c9761b22fa15ee3e374343aa0
Bernardo Franco T <72668671+BooleanEE@users.noreply.github.com>
2023-05-31 03:38:27 +0000

    Corrections.

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -24,29 +24,21 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
 });
 
-test('Testando cadastro de três respostas', () => {
+test('Testando o cadastro de três respostas conjuntamente get_respostas', () => {
   modelo.cadastrar_pergunta('Who is the main character in Legend of Heroes Trails in The Sky First Chapter?');
   modelo.cadastrar_pergunta('Who is the main character in Legend of Heroes Trails From Zero?');
   modelo.cadastrar_pergunta('Who is the main character in Legend of Heroes Trails Cold Steel?');
   modelo.cadastrar_resposta(4,'Estelle Bright');
-  modelo.cadastrar_resposta(4,'Joshua');
-  modelo.cadastrar_resposta(4,'Kloe Rinz');
   modelo.cadastrar_resposta(5,'Lloyd Bannings');
   modelo.cadastrar_resposta(6,'Ren Schwarz');
-  const perguntas = modelo.listar_perguntas();
   expect(modelo.get_respostas(4)[0]["texto"]).toBe('Estelle Bright');
   expect(modelo.get_respostas(5)[0]["texto"]).toBe('Lloyd Bannings');
   expect(modelo.get_respostas(6)[0]["texto"]).toBe('Ren Schwarz');
-  expect(perguntas[0].num_respostas).toBe(3);
 });
 
-test('Testando coerência do identificador de pergunta', () => {
+test('Testando coerência do identificador de pergunta (get_pergunta)', () => {
   modelo.cadastrar_pergunta('Which is the best track in Trails Series in your opinion?');
-  modelo.cadastrar_pergunta('Which is the best character in Trails Series in your opinion?');
-  modelo.cadastrar_pergunta('Which is the best villain in Trails Series in your opinion?');
-  expect(modelo.get_pergunta(7)["id_pergunta"]).toBe(7);
-  expect(modelo.get_pergunta(7)["id_usuario"]).toBe(1);
-  expect(modelo.get_pergunta(7)["texto"]).toBe('Which is the best track in Trails Series in your opinion?');
-  expect(modelo.get_pergunta(8)["texto"]).toBe('Which is the best character in Trails Series in your opinion?');
-  expect(modelo.get_pergunta(9)["texto"]).toBe('Which is the best villain in Trails Series in your opinion?');
+  const perguntas_axl = modelo.listar_perguntas();
+  const perguntas_get = modelo.get_pergunta(perguntas_axl[0].id_pergunta)
+  expect(perguntas_get.texto).toBe('Which is the best track in Trails Series in your opinion?');
 });
